f623948cbad278b05c815d857caee3f61ee1bd76
Louis Bouchard <louis.bouchard@canonical.com>
2017-03-30 17:00:04 +0200

    [sponsor_backlog] Fix the script with new BugTasks logic
    Signed-off-by: Louis Bouchard <louis.bouchard@canonical.com>

--- sponsor_backlog.py
+++ sponsor_backlog.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 #
-# sponsors_backlog : Get a list of LP bugs with sts-sponsors tag
+# sts-sru : Get a list of LP bugs with sts-sru tag
 #
 # Copyright (C) 2016 Louis bouchard <louis.bouchard@canonical.com>
 #
@@ -9,16 +9,30 @@ import argparse
 from BugTasks import BugTasks
 
 
-def main(long):
-    sponsors = BugTasks()
-    sponsors.login()
-    sponsors.get_all_tasks('sts-sponsor')
-    sponsors.display_report(long, 'sts-sponsor')
+def main(arguments, tag):
+    sru = BugTasks()
+    sru.login()
+    if arguments.ubuntu:
+        sru.get_ubuntu_tasks(tag)
+    if arguments.uca:
+        sru.get_uca_tasks(tag)
+    if arguments.openstack:
+        sru.get_openstack_tasks(tag)
+
+    sru.display_report(arguments.long, tag)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-l', '--long', help='Display series and owners',
                         action='store_true')
+    parser.add_argument('-o', '--openstack', help='Display openstack bugs\
+                        this can be a lengthy process as it needs to scan\
+                        all openstack projects', action='store_true')
+    parser.add_argument('-u', '--ubuntu', help='Display Ubuntu bugs',
+                        action='store_true')
+    parser.add_argument('-U', '--uca', help='Display Ubuntu Cloud Archive\
+                        bugs', action='store_true')
     args = parser.parse_args()
-    main(args.long)
+
+    main(args, 'sts-sponsor')
